e911f5a61af9bdf6a0054e112bce309977267dbc
HarrisonCTEC <harrison.ctec@gmail.com>
2015-02-26 08:14:20 -0700

    Added SpringLayout to DatabasePanel

--- src/harrisonctec/view/DatabasePanel.java
+++ src/harrisonctec/view/DatabasePanel.java
@@ -3,11 +3,24 @@ package harrisonctec.view;
 import harrisonctec.controller.DatabaseController;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SpringLayout;
 
 public class DatabasePanel extends JPanel
 {
+	private DatabaseController baseController;
+	private SpringLayout baseLayout;
+	
 	public DatabasePanel(DatabaseController baseController)
 	{
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
 		
+		setupPanel();
+	}
+
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
 	}
 }
